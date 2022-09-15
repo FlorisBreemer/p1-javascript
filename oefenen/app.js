976f0784cbc8a871010bdb1ac4f7c8d650f62206
FlorisBreemer <9018644@student.zadkine.nl>
2022-09-15 14:57:00 +0200

    javascript 3.1

--- oefenen/app.js
+++ oefenen/app.js
@@ -7,3 +7,15 @@ document.write(myName)
 document.write(myAge)
 document.write(diploma)
 document.write(passed) 
+
+let num1 = 70;
+let num2 = 69;
+let answer = num1 + num2
+let answer2 = num1 * num2;
+let answer3 = num1 - num2;
+let answer4 = num1 / num2; 
+
+console.log(answer)  
+console.log(answer2)
+console.log(answer3)
+console.log(answer4)
