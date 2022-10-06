22c578463ce57b40c40395dd3cf76e40613058cb
FlorisBreemer <9018644@student.zadkine.nl>
2022-10-06 14:20:16 +0200

    opdracht 6.4

---
+++ opdracht 6.4/app.js
@@ -0,0 +1,34 @@
+let x1 = calculator(1, 12);
+
+function calculator(a, b) {
+  return a + b;
+}
+
+alert(x1)
+
+
+let x2 = multiplyByFive(1);
+
+function multiplyByFive(a) {
+  return a * 5;
+}
+
+alert(x2)
+
+
+let x3 = checkInput(50, 12);
+
+function checkInput(a, b) {
+  return Math.max(a, b);
+}
+
+alert(x3)
+
+
+let x4 = calculateMinutes(3600);
+
+function calculateMinutes(a) {
+    return a / 60;
+}
+
+alert(x4)
