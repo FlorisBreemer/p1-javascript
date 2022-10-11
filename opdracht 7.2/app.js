e98fa4db305560e0eaec1c9976ab9ef5a7b8b26c
FlorisBreemer <9018644@student.zadkine.nl>
2022-10-11 12:05:10 +0200

    opdracht 7.2

---
+++ opdracht 7.2/app.js
@@ -0,0 +1,12 @@
+let budget = 100
+let product = 60
+let vraag = prompt("Hoeveel kost het product dat je wilt kopen?")
+
+if (vraag <=100) {
+    document.write("U heeft genoeg geld!")
+    
+} else {
+    document.write("Helaas, te weinig geld")
+}
+
+
