49d14529dc02bde22f41296df4c873fb6279a44c
FlorisBreemer <9018644@student.zadkine.nl>
2022-11-04 11:23:45 +0100

    opdracht 9.1

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -6,10 +6,13 @@ const g2 = document.getElementById("nummer2")
 function multiply(){
     p.innerText = `antwoord = ${g1.value * g2.value}`;;
 }
+if ("antwoord" <= 0)
+
 function divide(){
     let divv = g1.value / g2.value;
     p.innerText = 'antwoord =' + divv;
 }
+
 function add(){
     const g1 = document.getElementById("nummer1").value
     const g2 = document.getElementById("nummer2").value
@@ -18,6 +21,7 @@ function add(){
 
     p.innerText = 'antwoord =' + test ;
 }
+
 function minus(){
     let minuss = g1.value - g2.value;
     p.innerText = 'antwoord =' + minuss
